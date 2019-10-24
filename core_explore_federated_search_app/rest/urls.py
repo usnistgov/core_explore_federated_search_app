d08c402f83a62e4f1038dd45209e94cddad4ea68
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-24 14:50:07 -0400

    fix: revert django upgrade
    Change-Id: Id8e4bf573b686d822405211915d3b6629dd69ada

--- core_explore_federated_search_app/rest/urls.py
+++ core_explore_federated_search_app/rest/urls.py
@@ -1,14 +1,13 @@
 """ Url router for the REST API
 """
-
-from django.urls import re_path
+from django.conf.urls import url
 
 from core_explore_federated_search_app.rest.query import views as query_views
 from core_explore_federated_search_app.rest.result import views as result_views
 
 urlpatterns = [
-    re_path(r'^execute-query', query_views.QueryExecute.as_view(),
-            name='core_explore_federated_search_app_rest_execute_query'),
-    re_path(r'^result', result_views.ResultDetail.as_view(),
-            name='core_explore_federated_search_app_rest_get_result_from_data_id'),
+    url(r'^execute-query', query_views.QueryExecute.as_view(),
+        name='core_explore_federated_search_app_rest_execute_query'),
+    url(r'^result', result_views.ResultDetail.as_view(),
+        name='core_explore_federated_search_app_rest_get_result_from_data_id'),
 ]
