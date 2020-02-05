8fbd8dbae8572635c0dac2e7fcb5357cd4aee7f8
Augustin CHINI <augustin.chini@nist.gov>
2020-02-05 10:06:46 -0500

    chore: upgrade 1.8.0
    Change-Id: I0520d05327d01b6e28d260f17a644d4793ac44a4

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_explore_federated_search_app",
-    version="1.7.0",
+    version="1.8.0",
     description="Federation exploration functions for curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
