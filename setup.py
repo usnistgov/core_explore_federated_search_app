8bfd9fd686e8eba6abd71802b2e79b7c244e3b12
Guillaume Sousa <guillaume.sousa@nist.gov>
2024-01-17 21:13:04 +0000

    chore: upgrade 2.8.0
    Change-Id: I872d570997532573e36a1aa1842712e0237efc43

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_explore_federated_search_app",
-    version="2.7.0",
+    version="2.8.0",
     description="Federation exploration functions for curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
